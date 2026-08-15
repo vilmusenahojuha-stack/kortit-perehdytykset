5f0b5c2c96c034e98fbfac518dd97514a8daa22a
vilmusenahojuha-stack <vilmusenahojuha@gmail.com>
2026-08-15 11:40:33 +0300

    Päivitä PWA-välimuisti v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "kortit-perehdytykset-v1";
-const ASSETS = ["./", "index.html", "style.css", "app.js", "manifest.json", "icon.svg"];
+const CACHE = "kortit-perehdytykset-v2";
+const ASSETS = ["./", "index.html", "style.css", "app.js?v=2", "manifest.json", "icon.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", (event) => {
