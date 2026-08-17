b0abf2890faac509910172953d8a187e8ec83011
vilmusenahojuha-stack <vilmusenahojuha@gmail.com>
2026-08-17 10:29:08 +0300

    Päivitä PWA-välimuisti v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kortit-perehdytykset-v3";
+const CACHE = "kortit-perehdytykset-v4";
 const ASSETS = ["./", "index.html", "style.css", "app-v3.js", "manifest.json", "icon.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
